5034786fdbc470a9f854a05c879a96c38b356195
Apollo <1983631067@qq.com>
2024-09-17 22:57:28 +0800

    [any message]

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ def main():
     Content_length=len(target)-6
     #parsing request header
     request_header=''
-    crif=[index for index in len(request) if request[index:index+4]=='\r\n' ]
+    crif=[index for index in range(len(request)) if request[index:index+4]=='\r\n' ]
     request_header=request[crif[0]+3:crif[:-1]]
     header_dict={}
     items=re.findall(r'(\w+): (.*?)\r\n',request_header+'\r\n')

394dbaca5b939ef98c44aeeb4fe3ce4c03873653
Apollo <1983631067@qq.com>
2024-09-17 21:26:35 +0800

    [any message]

--- app/main.py
+++ app/main.py
@@ -6,8 +6,15 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    client_socket,client_addr=server_socket.accept() # wait for client
+    response=b''
+    status=b'HTTP/1.1 200 OK\r\n'
+    headers=b'\r\n'
+    body=b''
+    response+=status
+    response+=headers
+    response+=body
+    client_socket.sendall(response)
 
 if __name__ == "__main__":
     main()

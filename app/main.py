41372cb125463b6f3d79738024cfc6b2febde3a3
Apollo <1983631067@qq.com>
2024-09-18 15:48:43 +0800

    [any message]

--- app/main.py
+++ app/main.py
@@ -94,7 +94,7 @@ def handle_request(client_socket:socket.socket):
     
     if Content_Encoding=='gzip':
             try:
-                content=gzip.compress(data=content.encode('utf-8'))
+                content=str(gzip.compress(data=content.encode('utf-8')))
                 d_content=gzip.decompress(data=content)
                 print(f'content={content},d-content={d_content}')
             except:

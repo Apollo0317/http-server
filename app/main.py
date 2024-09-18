a1a2d44b7a3017faf6f49bbf60e9e4200ea6fc8e
Apollo <1983631067@qq.com>
2024-09-18 12:52:02 +0800

    [any message]

--- app/main.py
+++ app/main.py
@@ -83,7 +83,7 @@ def handle_request(client_socket:socket.socket):
     #form response status
     status=bytes('HTTP/1.1 {} {}\r\n'.format(code,status_describe),encoding='utf-8')
     #form response header
-    headers=bytes(f'Content-Type:{Content_Type}\r\n Content-Length:{Content_length}\r\n\r\n',encoding='utf-8')
+    headers=bytes(f'Content-Type: {Content_Type}\r\nContent-Length: {Content_length}\r\n\r\n',encoding='utf-8')
     #form response body
     body=bytes(f'{content}',encoding='utf-8')
     response+=status

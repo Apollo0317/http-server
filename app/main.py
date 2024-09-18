4c87ce43c8b7e9b2cf93f569e0a2e27e72bebae3
Apollo <1983631067@qq.com>
2024-09-18 11:33:00 +0800

    [any message]

--- app/main.py
+++ app/main.py
@@ -37,7 +37,7 @@ def handle_request(client_socket:socket.socket):
         status_describe='OK'
         content=request_header['User-Agent']
         Content_length=len(content)
-    elif target[:6]==r'/files':
+    elif '/files' in target:
         filename=target[7:]
         with open(file=filename,encoding='utf-8') as f:
             f.write(content)
@@ -69,7 +69,8 @@ def main():
     with ThreadPoolExecutor(max_workers=5) as executor:
         while True:
             client_socket,client_addr=server_socket.accept() # wait for client
-            print(f'new connecton:addr={client_addr}')
+            ip=client_addr[0]+':'+str(client_addr[1])
+            print(f'new connecton:addr={ip}')
             executor.submit(handle_request,client_socket)
 
 
